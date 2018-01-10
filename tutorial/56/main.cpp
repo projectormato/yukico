c7fb7e7321456880257f2ee37fd3a2cf6f1e04ba
projectormato <k.keita811.anuy@gmail.com>
2018-01-10 22:48:52 +0900

    add 56 code

---
+++ tutorial/56/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define step(i, s, n, d) for(int i=s; i<n; i+=d)
+#define FOR(i,s,n) step(i,s,n,1)
+#define rep(i,n) FOR(i,0,n)
+#define gets(x) x; cin >> x;
+#define puts(x) x; cout << x << endl;
+#define list_input(x, n) x[n]; rep(i,n){ gets(x[i]);}
+#define ll long long
+
+int main(){
+  ll d, p;
+  cin >> d >> p;
+  puts(d+(d*p/100))
+}
