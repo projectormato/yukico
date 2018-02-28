d0d2197910c1f167c0f2f36cc4946e9f226236d6
projectormato <k.keita811.anuy@gmail.com>
2018-03-01 01:01:47 +0900

    add 311 code

---
+++ level1_5/311/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define step(i, s, n, d) for(int i=s; i<n; i+=d)
+#define FOR(i,s,n) step(i,s,n,1)
+#define rep(i,n) FOR(i,0,n)
+#define gets(x) x; cin >> x;
+#define puts(x) x; cout << x << endl;
+#define list_input(x, n) x[n]; rep(i,n){ gets(x[i]);}
+#define ll long long
+
+int main(){
+  ll n, result = 0;
+  cin >> n;
+  result += n/3;
+  result += n/5;
+  puts(result*2)
+}
